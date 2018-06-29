ab527682e923876b58982f7b2e3da7d2db54f588
Jeffrey Crockett <jeffreyneilcrockett@gmail.com>
2018-06-29 18:51:49 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -10,18 +10,19 @@ def argue(ment)
   ment
 end
 
-def greeting
+def greeting(hello, there)
 
 end
 
 def return_a_value
+  "Nice"
 
 end
 
 def last_evaluated_value
-
+  "expert"
 end
 
-def pizza_party
-
+def pizza_party(topping="cheese")
+  topping
 end
